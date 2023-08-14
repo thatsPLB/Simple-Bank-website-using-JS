3dba74225ce38e0f09e09f99703b6aa7e8cd37ad
Plabon Saha <sahaplabon66@gmail.com>
2023-08-15 04:10:26 +0600

    hsh

--- js/deposite.js
+++ js/deposite.js
@@ -1,12 +1,14 @@
 document.getElementById('btn-deposite').addEventListener('click',function(){
     const depositefield = document.getElementById('deposite-field');
-    const newDepositAmount = depositefield.value;
-    console.log(typeof newDepositAmount)
+    const newDepositAmountString = depositefield.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
+    
     const depositTotalEliment = document.getElementById('deposite-total');
-    const previusDepositTotal = depositTotalEliment.innerText
-    const currentDepositeTotal = previusDepositTotal + newDepositAmount
-
-    depositTotalEliment.innerText = currentDepositeTotal
+    const previusDepositTotalString = depositTotalEliment.innerText;
+    const previusDepositTotal = parseFloat(previusDepositTotalString);
+    console.log(typeof previusDepositTotal)
+    const currentDepositeTotal = previusDepositTotal + newDepositAmount;
+    depositTotalEliment.innerText = currentDepositeTotal;
     depositefield.value ='';
 
 })
